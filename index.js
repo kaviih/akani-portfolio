3fda58d7e62f7a6c2059cb498dc8ee937bd6c83e
Akani <akanihlongwane7@gmail.com>
2023-02-18 11:32:31 +0200

    added contact modal

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-/*BACKGROUND COLOUR CHANGING*/
+//MODAL
 
+//get the modal
+var modal = document.getElementById("modal");
+
+//get the button that opens the modal
+var btn = document.getElementById("openModalBtn");
+
+//get the <span> element that closes the modal
+var  btn =document.getElementById("contact__submit");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+// When the user clicks on <span> (x), close the modal
+btn.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
